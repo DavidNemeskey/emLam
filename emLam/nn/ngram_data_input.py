c0c373314e2679459c96960544a24a6f03d7dbca
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-04-05 10:42:33 +0200

    Now it works.

--- emLam/nn/ngram_data_input.py
+++ emLam/nn/ngram_data_input.py
@@ -4,7 +4,6 @@
 """Data readers that processes ngram count and model files."""
 
 from builtins import filter, map
-from itertools import islice
 import logging
 import random
 
@@ -41,6 +40,46 @@ class NgramModelLoader(NgramLoader):
 
 
 class NgramCountLoader(NgramLoader):
+    """
+    Gets the data from the count file. This implementation holds the whole
+    index array for the ngrams in memory, but at least it is fast.
+    """
+    def __init__(self, *args):
+        super(NgramCountLoader, self).__init__(*args)
+        self.ngrams, self.freqs = self.__read_file()
+        self.data_len = self.freqs.sum()
+        self.epoch_size = self.data_len // self.batch_size
+        self.seed = 42
+        self.indices = self.__fill_indices()
+
+    def __fill_indices(self):
+        """For the generation."""
+        indices = np.zeros(self.data_len, dtype=np.int32)
+        last_index = 0
+        for i, f in enumerate(self.freqs):
+            indices[last_index:last_index + f] = i
+            last_index += f
+        rnd = np.random.RandomState(self.seed)
+        rnd.shuffle(indices)
+        return indices
+
+    def __iter__(self):
+        last = 0
+        for e in range(self.epoch_size):
+            yield self.ngrams[self.indices[last:last + self.batch_size]]
+            last += self.batch_size
+
+    def __read_file(self):
+        with openall(self.ngram_file, 'rt') as inf:
+            data_it = filter(lambda ngf: ngf[0].count(' ') == self.order - 1,
+                             map(lambda l: l.rstrip().split('\t'), inf))
+            data = [(np.array([self.vocab[word] for word in ngram.split(' ')],
+                              dtype=np.int32),
+                     int(freq)) for ngram, freq in data_it]
+            return map(np.array, zip(*data))
+
+
+class SlowNgramCountLoader(NgramLoader):
     """Gets the data from the count file."""
     def __init__(self, *args):
         super(NgramCountLoader, self).__init__(*args)
@@ -63,79 +102,6 @@ class NgramCountLoader(NgramLoader):
                 ngrams[i] = self.data[ins][0]
                 aggr_freqs[ins:] -= 1
             yield ngrams
-
-    def __random_it(self):
-        """
-        Generates random numbers between 0 and the current maximum data size
-        (as we draw without replacement).
-        """
-        rnd = random.Random()
-        rnd.seed(self.seed)
-        for r in range(self.data_len - 1, -1, -1):
-            yield rnd.randint(0, r)
-
-    def __read_file(self):
-        with openall(self.ngram_file, 'rt') as inf:
-            data_it = filter(lambda ngf: ngf[0].count(' ') == self.order - 1,
-                             map(lambda l: l.rstrip().split('\t'), inf))
-            data = [(np.array([self.vocab[word] for word in ngram.split(' ')],
-                              dtype=np.int32),
-                     int(freq)) for ngram, freq in data_it]
-            return data
-
-
-class NgramCountLoaderOld(NgramLoader):
-    """Gets the data from the count file."""
-    def __init__(self, *args):
-        super(NgramCountLoaderOld, self).__init__(*args)
-        self.data = self.__read_file()
-        self.data_len = sum(freq for _, freq in self.data)
-        self.epoch_size = self.data_len // self.batch_size
-        self.seed = 42
-
-    def __iter__(self):
-        batch_it = self.__random_it()
-        data = [[ngram, freq] for ngram, freq in self.data]
-        for i in range(self.epoch_size):
-            batch_ids = islice(batch_it, self.batch_size)
-            batch_ids = self.__transform_batch_ids(batch_ids)
-            ngrams = self.__get_ngrams(batch_ids, data)
-            yield ngrams
-
-    @staticmethod
-    def __transform_batch_ids(batch_ids):
-        """
-        Transforms the batch ids so that removal (draws without replacement)
-        are handled correctly.
-        """
-        a = np.array(list(batch_ids))
-        s = set()
-        for i, e in enumerate(a):
-            while e in s:
-                e += 1
-            s.add(e)
-            a[i + 1:] = np.where(a[i + 1:] >= e, a[i + 1:] + 1, a[i + 1:])
-            a[i] = e
-        return sorted(a)
-
-    def __get_ngrams(self, batch_ids, data):
-        """Gets the n-grams that correspond to the batch ids."""
-        to_delete = []
-        ngrams = []
-        datait = enumerate(data)
-        datai, datap = next(datait)
-        curr_freq = datap[1]
-        for batch_id in batch_ids:
-            while batch_id >= curr_freq:
-                datai, datap = next(datait)
-                curr_freq += datap[1]
-            datap[1] -= 1
-            if datap[1] == 0:
-                to_delete.append(datai)
-            ngrams.append(datap[0])
-        for i in to_delete[::-1]:
-            data.pop(i)
-        return ngrams
 
     def __random_it(self):
         """
