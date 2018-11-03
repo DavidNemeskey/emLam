bd4bda0aeaa79d22204a796961f9f9b48bd13c52
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-11-03 16:24:11 +0100

    Added a new field converter function: real_lemma_inf.

--- emLam/__init__.py
+++ emLam/__init__.py
@@ -1,1 +1,4 @@
-WORD, LEMMA, POS, LEMMA_POS = 0, 1, 2, -1
+# Positions of the various fields in CoNLL-like formats I extract from GATE.
+WORD, LEMMA, POS, ANAS = 0, 1, 2, 3
+# LEMMA_POS only occurs in the Szeged Corpus
+LEMMA_POS = -1
--- emLam/conversions.py
+++ emLam/conversions.py
@@ -7,21 +7,25 @@ from operator import itemgetter
 import re
 import sys
 
-from emLam import WORD, LEMMA, POS
+from emLam import WORD, LEMMA, POS, ANAS
 
 
-_univ_re = re.compile(r'([[][^[]+[]])')
-_univ_pos_re = re.compile(r'/([^]]+)\]|(Adj\|nat)\]')
-_univ_hyph_re = re.compile(r'^\[Hyph:[^]]+\]|\[Punct\]$')
+_pos_str = r'(\[[^[+]+\])'
+_pos_re = re.compile(_pos_str)
+_pos_pos_re = re.compile(r'/([^]]+)\]|(Adj\|nat)\]')
+_pos_hyph_re = re.compile(r'^\[Hyph:[^]]+\]|\[Punct\]$')
 # To fix erroneous tagging
-_univ_map = {
+_pos_map = {
     '[N]': '[/N]',
     '[V]': '[/V]',
     '[Num]': '[/Num]',
     '[_Mod]': '[_Mod/V]',
 }
-# Drop the 'default' inflections (zero morphemes)
-_univ_drop = set(['[Nom]', '[Prs.NDef.3Sg]'])
+# Drop the 'default' inflections (zero morphemes) + degenerate tags
+_pos_drop = {'[Nom]', '[Prs.NDef.3Sg]', '[]'}
+
+# _anas_eq = re.compile(r'(?P<deep>.+?)(?P<tag>\[[^]]+\])(?:=(?P<surface>.+?))?')
+_anas_eq = re.compile(r'(?:([^+]+){0}(?:=([^+]+))?)|(?:(\+){0}=(\+))|(?:(){0}())'.format(_pos_str))
 
 
 def field_word(fields):
@@ -39,23 +43,23 @@ def field_full_pos(fields):
 
 def field_pos_inf(fields):
     """The POS tag + the inflections as separate tokens."""
-    univ = fields[POS]
-    if '[' in univ and not _univ_hyph_re.match(univ):  # not OTHER, maybe sth else too
+    pos = fields[POS]
+    if '[' in pos and not _pos_hyph_re.match(pos):  # not OTHER, maybe sth else too
         pos_ana = [None]
         infl_ana = []
         form = 0
-        parts = _univ_re.findall(univ)
+        parts = _pos_re.findall(pos)
         for part in parts:
-            part = _univ_map.get(part, part)
+            part = _pos_map.get(part, part)
             if part == '[/Supl]':  # Delete 'leg', but remember it
                 form = 2
             elif part.startswith('[_Comp'):
                 form = max(form, 1)
-            posm = _univ_pos_re.search(part)
+            posm = _pos_pos_re.search(part)
             if posm:
                 pos_ana[0] = posm.group(1) or posm.group(2)
             else:
-                if part not in _univ_drop:
+                if part not in _pos_drop:
                     infl_ana.append(part)
         if form == 1:
             pos_ana.append('[Comp]')
@@ -132,7 +136,7 @@ def field_pos_kr(fields):
 
 def field_lemma_inf(fields):
     """
-    For universal dependencies returned by GATE: ~lemmad_krs.
+    For POS tags returned by GATE: ~lemmad_krs.
     Fields: word, lemma, pos (, ...).
     """
     return _field_lemma_inf(fields)
@@ -140,7 +144,7 @@ def field_lemma_inf(fields):
 
 def field_lemma_inf_with_pos(fields):
     """
-    For universal dependencies returned by GATE: ~lemmad_krs, but the POS tag
+    For POS tags returned by GATE: ~lemmad_krs, but the POS tag
     (in the narrow sense, i.e. /N, etc.) is appended to the lemma as well.
     Fields: word, lemma, pos (, ...).
     """
@@ -149,14 +153,14 @@ def field_lemma_inf_with_pos(fields):
 
 def _field_lemma_inf(fields, keep_pos=False):
     lemma = fields[LEMMA]
-    univ = fields[POS]
+    pos = fields[POS]
     word_ana = [lemma]
     infl_ana = []
-    if '[' in univ and not _univ_hyph_re.match(univ):  # not OTHER, maybe sth else too
+    if '[' in pos and not _pos_hyph_re.match(pos):  # not OTHER, maybe sth else too
         form = 0
-        parts = _univ_re.findall(univ)
+        parts = _pos_re.findall(pos)
         for part in parts:
-            part = _univ_map.get(part, part)
+            part = _pos_map.get(part, part)
             if part == '[/Supl]':  # Delete 'leg', but remember it
                 form = 2
             if part.startswith('[/'):  # POS tag
@@ -167,13 +171,82 @@ def _field_lemma_inf(fields, keep_pos=False):
             elif part.startswith('[_'):
                 word_ana[-1] += part
             else:
-                if part not in _univ_drop:
+                if part not in _pos_drop:
                     infl_ana.append(part)
         if form == 1:
             word_ana.append('[Comp]')
         elif form == 2:
             word_ana.append('[Supl]')
     return word_ana + infl_ana
+
+
+def field_real_lemma_inf(fields):
+    """
+    For POS tags returned by GATE. The main difference between this and
+    lemma_inf is that the derivational suffixes are not exposed; instead, their
+    "surface" forms are fused with the lemma. In other words, this function
+    always returns valid words as lemmas, while lemma_inf does not.
+    """
+    word, lemma, pos, anas = fields[WORD], fields[LEMMA], fields[POS], fields[ANAS]
+    anas = json.loads(anas)
+    # not OTHER, hyphen, or unanalyzed word
+    if '[' in pos and not _univ_hyph_re.match(pos) and anas:
+        # Filter to the analyses selected by emLemma
+        anas = [ana for ana in anas if ana['lemma'] == lemma and ana['feats'] == pos]
+        for ana in anas:
+            parts = _split_ana(ana)
+            ret_lemma, ret_infl = _reconstruct_lemma_inf(parts)
+            return [ret_lemma] + ret_infl
+    else:
+        return word
+
+
+def _split_ana(ana):
+    """
+    Returns the list of tag -- deep form -- surface form tuples in the analysis.
+    """
+    ret = []
+    for part in _anas_eq.finditer(ana['ana']):
+        groups = part.groups()
+        if groups[1]:
+            deep, tag = groups[0:2]
+            surface = groups[2] if groups[2] else groups[0]
+        elif groups[4]:
+            deep, tag, surface = groups[3:6]
+        elif groups[7]:
+            deep, tag, surface = groups[6:9]
+        else:
+            assert 'This should not happen: {}'.format(part.groups())
+
+
+def _reconstruct_lemma_inf(ana_parts):
+    """
+    Reconstructs the lemma and inflection based on the ana
+    parts (POS tag -> surface form mapping) we extracted from the analysis.
+
+    Note that the proper handling of comparative and superlative forms of
+    needs more work, and is not handled by the current code.
+    """
+    lemma = []
+    inf = []
+
+    # For the last non-inflection tag, we must keep the deep ("lemmatized")
+    # form, so that the word doesn't end in a linking/modified vowel
+    keep_surface = False
+    for ana_tag, ana_deep, ana_surface in ana_parts[::-1]:
+        # Note: 
+        if ana_tag.startswith('[/') or ana_tag.startswith('[_'):
+            # POS tag or derivation: fuse with lemma
+            lemma.append(ana_surface if keep_surface else ana_deep)
+            keep_surface = True
+        elif ana_tag == '[]':
+            # Degenerate
+            lemma.append(ana_surface if keep_surface else ana_deep)
+            keep_surface = True
+        elif ana_tag not in _pos_drop:
+            inf.append(ana_tag)
+
+    return ''.join(lemma[::-1]), inf[::-1]
 
 
 def _get_field_selectors():
